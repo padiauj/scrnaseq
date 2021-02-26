fa4c0bd7a2e6adfd49015b7fbfbb07f99acbf6f3
Ubuntu <ubuntu@ip-172-30-6-99.ec2.internal>
2021-02-26 05:36:37 +0000

    Make each run of the program process one sample

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,7 @@ RUN pip3 install --no-cache-dir kb-python
 RUN mkdir ~/.aws
 RUN echo "[default]\nregion = us-east-1" > ~/.aws/config
 RUN aria2c https://cf.10xgenomics.com/misc/bamtofastq-1.3.2
+RUN chmod +x bamtofastq-1.3.2
+RUN kb ref -d human -i kb_human.idx -g kbtg.txt
+RUN aria2c https://cf.10xgenomics.com/misc/bamtofastq-1.3.2
 COPY run.py .
--- run.py
+++ run.py
@@ -6,10 +6,21 @@ from datetime import datetime
 
 logger = logging.getLogger("run")
 
-DRY = False
+DEFAULT_DRY = False
 
 
-def run_cmd(params, dry=DRY):
+def get_file_order(lst):
+    final = []
+    for fastq in lst:
+        if ("_R1_" in fastq):
+            r2 = fastq.replace("_R1_", "_R2_")
+            if (r2 in lst):
+                final.append(fastq)
+                final.append(r2)
+    return final
+
+
+def run_cmd(params, dry=DEFAULT_DRY):
     if (not dry):
         proc = subprocess.run(params, capture_output=True)
         logger.info(proc.stdout.decode('utf-8'))
@@ -19,77 +30,101 @@ def run_cmd(params, dry=DRY):
 
 def init_logger():
     timestr = datetime.now().strftime("%Y%m%d-%H%M%S.%f")
-    formatter = logging.Formatter()
-    logging.basicConfig(filename="output_{time}.log".format(
-        time=timestr, encoding='utf-8', level=logging.DEBUG),
+    fname = "output_{time}.log".format(time=timestr, encoding='utf-8')
+    logging.basicConfig(filename=fname,
+                        level=logging.DEBUG,
                         format='%(asctime)s:%(levelname)s: %(message)s')
     logger.setLevel(logging.DEBUG)
-    return logger
+    logger.addHandler(logging.StreamHandler())
+    return logger, fname
+
+
+def name_str(name):
+    keepcharacters = (' ', '.', '_')
+    safe_name = "".join(c for c in name.strip()
+                        if c.isalnum() or c in keepcharacters).rstrip()
+    return safe_name
+
+
+def seq_link(link):
+    return link
 
 
 if __name__ == "__main__":
-    logger = init_logger()
+    logger, logpath = init_logger()
     parser = argparse.ArgumentParser(description="Deploy scrna analysis tasks")
+    parser.add_argument('name',
+                        metavar='name',
+                        type=name_str,
+                        help="Sample name to uniquely identify run")
     parser.add_argument(
-        'pairs',
-        metavar='name,link',
-        type=str,
-        nargs='+',
+        'link',
+        metavar='link',
+        type=seq_link,
         help=
         "Names and links to BAM files to process e.g. sample1,http://example.org/sample1.bam sample2,http://example.org/sample2.bam"
     )
+    parser.add_argument('--dry', dest='dry', action='store_true')
+    parser.set_defaults(dry=False)
+
     args = parser.parse_args()
+    nthreads = 16
 
-    # TODO check inputs
-    links = {
-        pair.split(",")[0].strip(): pair.split(",")[1].strip()
-        for pair in args.pairs
-    }
+    outbam = "{acc}.bam".format(acc=args.name)
+    fastqpath = "fastqs/{acc}".format(acc=args.name)
 
-    logger.debug("(name, link) pairs to process: " + str([
-        "({name}, {link})".format(name=name, link=link)
-        for name, link in links.items()
-    ]))
-    logger.info("Found {links} links to process.".format(links=len(links)))
+    # Fetch data
+    logger.info("Downloading {link} to {outbam}".format(link=args.link,
+                                                        outbam=outbam))
+    aria_call = ["aria2c", "-x", str(nthreads), "-o", outbam, args.link]
+    logger.info(" ".join(aria_call))
+    run_cmd(aria_call, dry=args.dry)
 
-    for acc in links:
-        outbam = "{acc}.bam".format(acc=acc)
-        fastqpath = "fastqs/{acc}".format(acc=acc)
+    # BAM to FASTQ Conversion
+    logger.info("Converting {outbam} to fastqs at path {outpath}".format(
+        outbam=outbam, outpath=fastqpath))
+    bamfastq_call = [
+        "./bamtofastq-1.3.2",
+        "--nthreads={nthreads}".format(nthreads=nthreads), outbam, fastqpath
+    ]
+    logger.info(" ".join(bamfastq_call))
+    run_cmd(bamfastq_call, dry=args.dry)
+    logger.info("Removing BAM...")
+    run_cmd(["rm", "-rf", outbam], dry=args.dry)
 
-        # Fetch data
-        logger.info("Downloading {link} to {outbam}".format(link=links[acc],
-                                                            outbam=outbam))
-        aria_call = ["aria2c", "-x", "16", "-o", outbam, links[acc]]
-        logger.info(" ".join(aria_call))
-        run_cmd(aria_call)
+    # Alignment
+    logger.info("Running Kallisto on {fastqpath}".format(fastqpath=fastqpath))
+    files = get_file_order(
+        sorted(glob.glob(fastqpath + "/gemgroup001/*_R*.fastq.gz")))
+    outpath = "out_{acc}".format(acc=args.name)
+    kb_call = [
+        "kb", "count", "-i", "kb_human.idx", "-g", "kbtg.txt", "-x", "10xv2",
+        "--h5ad", "--cellranger"
+    ] + files + ["-t", str(nthreads), "-o", outpath]
+    logger.info(" ".join(kb_call))
+    run_cmd(kb_call, dry=args.dry)
 
-        # BAM to FASTQ Conversion
-        logger.info("Converting {outbam} to fastqs at path {outpath}".format(
-            outbam=outbam, outpath=fastqpath))
-        bamfastq_call = [
-            "./bamtofastq-1.3.2", "--nthreads=16", outbam, fastqpath
-        ]
-        logger.info(" ".join(bamfastq_call))
-        run_cmd(bamfastq_call)
-        run_cmd(["rm", "-rf", outbam])
+    logger.info("Sync results with S3")
+    aws_sync_results = [
+        "aws",
+        "s3",
+        "sync",
+        outpath,
+        "s3://umesh-churchlab/{path}".format(path=outpath),
+    ]
+    logger.info(" ".join(aws_sync_results))
+    run_cmd(aws_sync_results, dry=args.dry)
+    aws_sync_log = [
+        "aws",
+        "s3",
+        "cp",
+        logpath,
+        "s3://umesh-churchlab/{path}".format(path=logpath),
+    ]
+    logger.info(" ".join(aws_sync_log))
 
-        # Alignment
-        logger.info(
-            "Running Kallisto on {fastqpath}".format(fastqpath=fastqpath))
-        files = sorted(glob.glob(fastqpath + "/gemgroup001/*_R*.fastq.gz"))
-        outpath = "out_{acc}".format(acc=acc)
-        kb_call = [
-            "kb", "count", "-i", "kallisto/kb_human.idx", "-g",
-            "kallisto/kbtg.txt", "-x", "10xv2", "--h5ad", "--report"
-        ] + files + ["-t", "16", "-o", "kallisto/{acc}".format(acc=acc)]
-        logger.info(" ".join(kb_call))
-        run_cmd(kb_call, dry=DRY)
+    run_cmd(aws_sync_log, dry=args.dry)
 
-        logger.info("Sync results with S3")
-        run_cmd([
-            "aws", "s3", "sync", outpath,
-            "s3://umesh-churchlab/{path}".format(path=outpath)
-        ])
-
-        logger.info("Cleaning up... ")
-        run_cmd(["rm", "-rf", fastqpath])
+    logger.info("Cleaning up... ")
+    run_cmd(["rm", "-rf", fastqpath], dry=args.dry)
+    run_cmd(["rm", "-rf", outpath], dry=args.dry)
